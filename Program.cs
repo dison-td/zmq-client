e044e69fdc7611909d3ac72eec198b4b6a690ec7
David Ison <david.ison@tirediscounters.com>
2023-04-26 10:36:34 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using NetMQ;
+using NetMQ.Sockets;
+
+using (var client = new RequestSocket())
+{
+    client.Connect("tcp://127.0.0.1:5556");
+    var count = 0;
+    while (true) 
+    {
+        client.SendFrame(String.Format("Hello {0}", count++));
+        var msg = client.ReceiveFrameString();
+        Console.WriteLine("From Server: {0}", msg);
+    }
+}
